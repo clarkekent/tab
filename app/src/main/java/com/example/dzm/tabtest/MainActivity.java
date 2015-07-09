9d2a81ebbcb31837d0f4df39d9f7705794c4ec8c
JD <546200957@qq.com>
2015-07-09 10:18:47 +0800

    add config for searchview

--- app/src/main/java/com/example/dzm/tabtest/MainActivity.java
+++ app/src/main/java/com/example/dzm/tabtest/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.dzm.tabtest;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 
 public class MainActivity extends Activity {
@@ -19,6 +21,15 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
